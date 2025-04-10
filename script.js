ca7696c900ec0e8870bd95ed54f2987dd2e7533d
BOWOENTERTAIMENT <bowomusicper@gmail.com>
2025-04-10 21:34:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function toggleDarkMode() {
-  document.body.classList.toggle("modo-claro");
+  const body = document.body;
+  const button = document.querySelector("button");
+
+  if (body.classList.contains("modo-cósmico")) {
+    body.classList.remove("modo-cósmico");
+    button.textContent = "Modo Cósmico";
+  } else {
+    body.classList.add("modo-cósmico");
+    button.textContent = "Modo Oscuro Místico";
+  }
 }
